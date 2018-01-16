6c05894e1a57820fe3522b9b163ce5626166151e
juankb1024 <jc.buitrago84@gmail.com>
2018-01-16 17:48:20 -0500

    updated jcommander for jdk6 flaw

--- src/main/java/com/beust/jcommander/internal/DefaultConverterFactory.java
+++ src/main/java/com/beust/jcommander/internal/DefaultConverterFactory.java
@@ -18,6 +18,13 @@
 
 package com.beust.jcommander.internal;
 
+import java.io.File;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URL;
+import java.util.Date;
+import java.util.Map;
+
 import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.IStringConverterFactory;
 import com.beust.jcommander.converters.BigDecimalConverter;
@@ -29,47 +36,40 @@ import com.beust.jcommander.converters.ISO8601DateConverter;
 import com.beust.jcommander.converters.IntegerConverter;
 import com.beust.jcommander.converters.LongConverter;
 import com.beust.jcommander.converters.StringConverter;
-//import com.beust.jcommander.converters.PathConverter;
 import com.beust.jcommander.converters.URIConverter;
 import com.beust.jcommander.converters.URLConverter;
 
-import java.io.File;
-import java.math.BigDecimal;
-import java.util.Date;
-import java.net.URI;
-import java.net.URL;
-//import java.nio.file.Path;
-import java.util.Map;
+public class DefaultConverterFactory implements IStringConverterFactory
+{
+	/**
+	 * A map of converters per class.
+	 */
+	private static Map<Class, Class<? extends IStringConverter<?>>> m_classConverters;
 
-public class DefaultConverterFactory implements IStringConverterFactory {
-  /**
-   * A map of converters per class.
-   */
-  private static Map<Class, Class<? extends IStringConverter<?>>> m_classConverters;
+	static
+	{
+		m_classConverters = Maps.newHashMap();
+		m_classConverters.put(String.class, StringConverter.class);
+		m_classConverters.put(Integer.class, IntegerConverter.class);
+		m_classConverters.put(int.class, IntegerConverter.class);
+		m_classConverters.put(Long.class, LongConverter.class);
+		m_classConverters.put(long.class, LongConverter.class);
+		m_classConverters.put(Float.class, FloatConverter.class);
+		m_classConverters.put(float.class, FloatConverter.class);
+		m_classConverters.put(Double.class, DoubleConverter.class);
+		m_classConverters.put(double.class, DoubleConverter.class);
+		m_classConverters.put(Boolean.class, BooleanConverter.class);
+		m_classConverters.put(boolean.class, BooleanConverter.class);
+		m_classConverters.put(File.class, FileConverter.class);
+		m_classConverters.put(BigDecimal.class, BigDecimalConverter.class);
+		m_classConverters.put(Date.class, ISO8601DateConverter.class);
+		m_classConverters.put(URI.class, URIConverter.class);
+		m_classConverters.put(URL.class, URLConverter.class);
+	}
 
-  static {
-    m_classConverters = Maps.newHashMap();
-    m_classConverters.put(String.class, StringConverter.class);
-    m_classConverters.put(Integer.class, IntegerConverter.class);
-    m_classConverters.put(int.class, IntegerConverter.class);
-    m_classConverters.put(Long.class, LongConverter.class);
-    m_classConverters.put(long.class, LongConverter.class);
-    m_classConverters.put(Float.class, FloatConverter.class);
-    m_classConverters.put(float.class, FloatConverter.class);
-    m_classConverters.put(Double.class, DoubleConverter.class);
-    m_classConverters.put(double.class, DoubleConverter.class);
-    m_classConverters.put(Boolean.class, BooleanConverter.class);
-    m_classConverters.put(boolean.class, BooleanConverter.class);
-    m_classConverters.put(File.class, FileConverter.class);
-    m_classConverters.put(BigDecimal.class, BigDecimalConverter.class);
-    m_classConverters.put(Date.class, ISO8601DateConverter.class);
-//    m_classConverters.put(Path.class, PathConverter.class);
-    m_classConverters.put(URI.class, URIConverter.class);
-    m_classConverters.put(URL.class, URLConverter.class);
-  }
-
-  public Class<? extends IStringConverter<?>> getConverter(Class forType) {
-    return m_classConverters.get(forType);
-  }
+	public Class<? extends IStringConverter<?>> getConverter(Class forType)
+	{
+		return m_classConverters.get(forType);
+	}
 
 }
